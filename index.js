3f4240c8ac6804fac70962d989b9e90d1392070b
Jannatul Ferdous Mirza <jfmirza1303@gmail.com>
2024-06-08 21:35:23 +0600

    added featured section functionality

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0pky6me.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -44,8 +44,30 @@ async function run() {
         res.send(result);
     })
 
-    // FEATURED SECTION
+    app.get('/products/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await productCollection.findOne(query);
+        res.send(result);
+    })
 
+    // FEATURED SECTION
+    app.get('/featured', async(req, res)=>{
+        const result = await productCollection.find({featured:true}).sort({"timestamp":-1}).toArray();
+        console.log(result);
+        res.send(result);
+    })
+
+    // INCREASE VOTE OF PRODUCT
+    app.patch('/products/:id', async(req, res)=>{
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)};
+        const updatedDoc = {
+            $inc: {upvote_count: 1},
+        }
+        const result = await productCollection.updateOne(filter, updatedDoc);
+        res.send(result);
+    })
 
 
     // Send a ping to confirm a successful connection
