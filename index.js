7683f1780aa38482ed155f8d69e5c9f9243575a3
Jannatul Ferdous Mirza <jfmirza1303@gmail.com>
2024-06-09 02:35:20 +0600

    added product details

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ async function run() {
         res.send(result);
     })
 
+    // PRODUCT DETAILS PAGE
     app.get('/products/:id', async(req, res)=>{
         const id = req.params.id;
         const query = {_id: new ObjectId(id)}
