d709b10731b956a2f0750be574937e897851d842
Jannatul Ferdous Mirza <jfmirza1303@gmail.com>
2024-06-17 04:28:10 +0600

    added corrections for deployment

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://tech-hive-d9f7e.web.app/",
+    "https://tech-hive-d9f7e.firebaseapp.com",
+  ]
+}));
 app.use(express.json());
 
 
@@ -27,7 +33,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
 
     const productCollection = client.db("techhiveDB").collection("products");
@@ -495,8 +501,8 @@ async function run() {
  
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
